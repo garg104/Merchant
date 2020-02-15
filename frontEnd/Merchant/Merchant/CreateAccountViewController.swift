621b0c949d1800c525919e01a1b07e7e775d4bcc
Domenic Conversa <domenicconversa@gmail.com>
2020-02-14 19:20:57 -0500

    Register view controllers UI implemented

--- frontEnd/Merchant/Merchant/CreateAccountViewController.swift
+++ frontEnd/Merchant/Merchant/CreateAccountViewController.swift
@@ -1,28 +1,24 @@
 //
-//  LogInViewController.swift
+//  CreateAccountViewController.swift
 //  Merchant
 //
-//  Created by Domenic Conversa on 2/12/20.
+//  Created by Domenic Conversa on 2/14/20.
 //  Copyright © 2020 CS307 Team 4. All rights reserved.
 //
 
 import UIKit
 
-class LogInViewController: UIViewController {
+class CreateAccountViewController: UIViewController {
 
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var loginButton: UIButton!
-    @IBOutlet weak var forgotPasswordButton: UIButton!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
-        addUnderlines() //add underlines to textfields
-        forgotPasswordButton.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0) //add forgotPasswordButton border
-        
+        addUnderlines()
     }
     
     func addUnderlines() {
@@ -33,12 +29,16 @@ class LogInViewController: UIViewController {
         let underLine2 = CALayer()
         underLine2.frame = CGRect(x: 0, y: usernameTextField.frame.height - 2, width: usernameTextField.frame.width, height: 2)
         underLine2.backgroundColor = UIColor.init(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0).cgColor
+        let underLine3 = CALayer()
+        underLine3.frame = CGRect(x: 0, y: usernameTextField.frame.height - 2, width: usernameTextField.frame.width, height: 2)
+        underLine3.backgroundColor = UIColor.init(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0).cgColor
         usernameTextField.borderStyle = .none
         usernameTextField.layer.addSublayer(underLine)
         passwordTextField.borderStyle = .none
         passwordTextField.layer.addSublayer(underLine2)
+        confirmPasswordTextField.borderStyle = .none
+        confirmPasswordTextField.layer.addSublayer(underLine3)
     }
-    
 
     /*
     // MARK: - Navigation
--- frontEnd/Merchant/Merchant/RegisterViewController.swift
+++ frontEnd/Merchant/Merchant/RegisterViewController.swift
@@ -14,6 +14,7 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
