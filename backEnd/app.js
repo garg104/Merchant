08062057b3ab4a42f6ce64712d1361e03923b61f
chirayu <garg104@purdue.edu>
2020-02-16 21:16:19 -0500

    Added User schema

--- backEnd/app.js
+++ backEnd/app.js
@@ -13,7 +13,9 @@ const userRouter = require('./routes/user');
 const app = express();
 
 //connection to MongoDB Atlas
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log("connected to the database")
+})
 
 //ADD database schemas: TODO
 
---
+++ backEnd/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('./node_modules/mongoose');
+
+
+const userSchema = mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    // add photo here - https://stackoverflow.com/questions/4796914/store-images-in-a-mongodb-database
+    university: {
+        type: String,
+        required: true,
+    },
+    sellingHistory: {
+        type: [Schema.Types.ObjectID],
+        ref: "Items",
+    },
+    forSale: {
+        type: [Schema.Types.ObjectID],
+        ref: "Items",
+    },
+    chats: {
+        type: Schema.Types.ObjectID,
+        ref: "Conversation",
+    },
+})
