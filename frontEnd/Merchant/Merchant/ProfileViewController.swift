98bf246c52ca9dd768add3c68a5497f8e5b35548
Drew Keirn <keirna@purdue.edu>
2020-02-17 10:20:28 -0500

    Added ProfileViewControlle.swift. This changed Main.storyboard and project.pbxproj (ignored?)

---
+++ frontEnd/Merchant/Merchant/ProfileViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ProfileViewController.swift
+//  Merchant
+//
+//  Created by Drew Keirn on 2/17/20.
+//  Copyright © 2020 CS307 Team 4. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var ProfilePicture: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
