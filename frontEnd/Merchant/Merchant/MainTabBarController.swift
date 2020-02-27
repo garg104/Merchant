445546d9da5695c137a1d1e2987b096d7ffd2393
Domenic Conversa <domenicconversa@gmail.com>
2020-02-27 01:22:33 -0500

    Implemented logging in functionality

---
+++ frontEnd/Merchant/Merchant/MainTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  MainTabBarController.swift
+//  Merchant
+//
+//  Created by Domenic Conversa on 2/27/20.
+//  Copyright © 2020 CS307 Team 4. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
