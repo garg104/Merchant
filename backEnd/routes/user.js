18ca05a99abdf3f4d6ad62c11652f71a62e8d400
aakarshitpandey <aakarshit.pandey7@gmail.com>
2020-02-28 00:22:13 -0500

    Updated the put route

--- backEnd/routes/user.js
+++ backEnd/routes/user.js
@@ -152,14 +152,14 @@ router.delete('/delete', async (req, res) => {
 })
 
 /* update user info */
-router.put('/', async (req, res) => {
+router.put('/username', async (req, res) => {
   try {
     //finding the user update the info
     //here update is a JSON which contains all the info to be updated
-    const ret = await User.findOneAndUpdate({ username: req.body.username }, { ...req.body.update })
+    const ret = await User.findOneAndUpdate({ username: req.body.username }, { username: req.body.newUsername })
     console.log(req.body)
     //sending a response to the user
-    res.status(200).json({ updated: { ...req.body.update }, msg: "The user settings have been updated" })
+    res.status(200).json({ updated: { ...req.body.newUsername }, msg: "The user settings have been updated" })
   } catch (e) {
     //sending an error response
     console.log(e)
