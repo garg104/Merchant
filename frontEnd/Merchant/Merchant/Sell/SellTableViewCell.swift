8d8243105592148ca7877884681edd484970d456
Domenic Conversa <domenicconversa@gmail.com>
2020-03-14 23:59:43 -0400

    Remove item UI implemented

---
+++ frontEnd/Merchant/Merchant/Sell/SellTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SellTableViewCell.swift
+//  Merchant
+//
+//  Created by Domenic Conversa on 3/14/20.
+//  Copyright © 2020 CS307 Team 4. All rights reserved.
+//
+
+import UIKit
+
+class SellTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var removeButton: UIButton!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
