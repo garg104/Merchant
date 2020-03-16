7b07fa1d11896ef9a53369cdc4da1549eaf2312d
chirayu <garg104@purdue.edu>
2020-03-15 21:11:35 -0400

    added route

--- backEnd/routes/items.js
+++ backEnd/routes/items.js
@@ -1,1 +1,23 @@
+require('dotenv').config()
+import { generateOtpMsg, sendEmail, generateTempPassword, generateResetPassword, generateDeleteAcctMsg } from '../utils/sendEmail'
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
+const express = require('express');
+const router = express.Router();
+import { config, getProfilePictureSchemas, parseFileData } from '../utils/fileHandling'
+const upload = config()
+const randomstring = require('../node_modules/randomstring')
+
+router.get('/', async (req, res) => {
+    try {
+      //get all the users in the DB
+      const users = await User.find({})
+      res.status(200).json({ users })
+    } catch (e) {
+      res.status(404).json({ msg: e.message })
+    }
+  });
+
+  module.exports = router;  
 
