5d0104d98967fb600d6fd238d2eafe11558d260e
Drew Keirn <keirna@purdue.edu>
2020-03-28 17:36:42 -0700

    fixed outlets and deleted old request

--- frontEnd/Merchant/Merchant/PostItemViewController.swift
+++ frontEnd/Merchant/Merchant/PostItemViewController.swift
@@ -11,6 +11,7 @@ import Alamofire
 
 class PostItemViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate {
     
+    
     // these next four functions are for the pickerView
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -41,15 +42,16 @@ class PostItemViewController: UIViewController, UIPickerViewDataSource, UIPicker
         categoryTextField.inputView = pickerView
     }
 
-    @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var postLabel: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var priceTextField: UITextField!
+    @IBOutlet weak var categoryTextField: UITextField!
+    @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var photosLabel: UILabel!
     @IBOutlet weak var photosScrollView: UIScrollView!
     @IBOutlet weak var choosePhotosButton: UIButton!
     @IBOutlet weak var doneButton: UIButton!
-    @IBOutlet weak var categoryTextField: UITextField!
     
     
     
@@ -77,46 +79,13 @@ class PostItemViewController: UIViewController, UIPickerViewDataSource, UIPicker
     
     @IBAction func postItem(_ sender: UIButton, completion: @escaping (_ validCode: Int)->()) {
         
-        // extract fields
-        struct parameters: Encodable {
-            var userID = 0
-            var name = ""
-            var desc = ""
-            var price = ""
-            var photos = 0
-            var category = ""
-            var isSold = false
-            var university = "Purdue University"
-        }
-        
-        let details = parameters(name: nameTextField.text!,
-                                 desc: descriptionTextView.text!,
-                                 price: priceTextField.text!)
-        
-        // make request
-        AF.request(API.URL + "/postItem",
-                   method: .post,
-                   parameters: details,
-                   encoder: URLEncodedFormParameterEncoder.default).response {response in
-                    
-                    let status = (response.response?.statusCode ?? 0)
-                    
-                    if (status != 0) {
-                        switch status {
-                        case 500: // error
-                            completion(status)
-                            break
-                        case 201: // success
-                            completion(status)
-                            break
-                        default:
-                            completion(status)
-                            break
-                        }
-                    }
-        }.resume()
+        // make upload request
+    
     }
     
+    @IBAction func choosePhotos(_ sender: UIButton) {
+        // choose photos from camera roll
+    }
     
 
     /*
