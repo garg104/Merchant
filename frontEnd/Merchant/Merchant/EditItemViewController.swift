11b5d75d75e1f5fd1171a45004277f902bf991d7
Drew Keirn <keirna@purdue.edu>
2020-03-29 17:27:15 -0700

    Added EditItemViewController and updated UI

---
+++ frontEnd/Merchant/Merchant/EditItemViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EditItemViewController.swift
+//  Merchant
+//
+//  Created by Drew Keirn on 3/29/20.
+//  Copyright © 2020 CS307 Team 4. All rights reserved.
+//
+
+import UIKit
+
+class EditItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
