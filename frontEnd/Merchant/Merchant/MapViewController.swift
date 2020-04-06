29d71e71d6bf3fbacf69e7b814bdd8b6e4d39b5c
Drew Keirn <keirna@purdue.edu>
2020-04-06 16:03:17 -0700

    added MapViewController

---
+++ frontEnd/Merchant/Merchant/MapViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MapViewController.swift
+//  Merchant
+//
+//  Created by Drew Keirn on 4/6/20.
+//  Copyright © 2020 CS307 Team 4. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
