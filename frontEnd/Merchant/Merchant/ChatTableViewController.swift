051c27bb0394f1d877be09fbc627c2fc7c84eea4
Drew Keirn <keirna@purdue.edu>
2020-04-09 18:10:09 -0700

    Added ChatTableViewContoller

---
+++ frontEnd/Merchant/Merchant/ChatTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChatTableViewController.swift
+//  Merchant
+//
+//  Created by Drew Keirn on 4/9/20.
+//  Copyright © 2020 CS307 Team 4. All rights reserved.
+//
+
+import UIKit
+
+class ChatTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
