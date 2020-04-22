488709cbef76c92a4874f5c9bb91dc1619b107a7
Domenic Conversa <domenicconversa@gmail.com>
2020-04-22 01:17:06 -0500

    Message bubbles chat UI implemented

--- frontEnd/Merchant/Merchant/Chat/ConversationViewController.swift
+++ frontEnd/Merchant/Merchant/Chat/ConversationViewController.swift
@@ -8,14 +8,44 @@
 
 import UIKit
 
-class ConversationViewController: UIViewController {
+struct ChatMessage {
+    let message: String
+    let isIncoming: Bool
+}
 
+class ConversationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     @IBOutlet weak var conversationTableView: UITableView!
+    
+    let messages = [
+        ChatMessage(message: "Hello", isIncoming: true),
+        ChatMessage(message: "Hey!", isIncoming: false),
+        ChatMessage(message: "What's up?", isIncoming: true),
+        ChatMessage(message: "This is a longer message that should wrap down to multiple lines", isIncoming: false)
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.conversationTableView.delegate = self
+        self.conversationTableView.dataSource = self
+        
+        conversationTableView.register(MessageTableViewCell.self, forCellReuseIdentifier: "messageCell")
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell", for: indexPath) as! MessageTableViewCell
+        cell.backgroundColor = .clear
+        cell.messageLabel.text = messages[indexPath.row].message
+        cell.isIncoming = messages[indexPath.row].isIncoming
+        
+        return cell
     }
     
 
