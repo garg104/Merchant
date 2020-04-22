694b10df19570fc8885f553730ff8dddf6aa7683
Domenic Conversa <domenicconversa@gmail.com>
2020-04-21 22:23:41 -0500

    Fixed detail images to show on all device sizes

--- frontEnd/Merchant/Merchant/Buy/BuyDetailViewController.swift
+++ frontEnd/Merchant/Merchant/Buy/BuyDetailViewController.swift
@@ -9,7 +9,55 @@
 import UIKit
 import Alamofire
 
+public extension UIDevice {
+
+    static let modelName: String = {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+
+        func mapToDevice(identifier: String) -> String { // swiftlint:disable:this cyclomatic_complexity
+            #if os(iOS)
+            switch identifier {
+            case "iPhone8,1":                               return "iPhone 6s"
+            case "iPhone8,2":                               return "iPhone 6s Plus"
+            case "iPhone9,1", "iPhone9,3":                  return "iPhone 7"
+            case "iPhone9,2", "iPhone9,4":                  return "iPhone 7 Plus"
+            case "iPhone8,4":                               return "iPhone SE"
+            case "iPhone10,1", "iPhone10,4":                return "iPhone 8"
+            case "iPhone10,2", "iPhone10,5":                return "iPhone 8 Plus"
+            case "iPhone10,3", "iPhone10,6":                return "iPhone X"
+            case "iPhone11,2":                              return "iPhone XS"
+            case "iPhone11,4", "iPhone11,6":                return "iPhone XS Max"
+            case "iPhone11,8":                              return "iPhone XR"
+            case "iPhone12,1":                              return "iPhone 11"
+            case "iPhone12,3":                              return "iPhone 11 Pro"
+            case "iPhone12,5":                              return "iPhone 11 Pro Max"
+            case "i386", "x86_64":                          return "Simulator \(mapToDevice(identifier: ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] ?? "iOS"))"
+            default:                                        return identifier
+            }
+            #elseif os(tvOS)
+            switch identifier {
+            case "AppleTV5,3": return "Apple TV 4"
+            case "AppleTV6,2": return "Apple TV 4K"
+            case "i386", "x86_64": return "Simulator \(mapToDevice(identifier: ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] ?? "tvOS"))"
+            default: return identifier
+            }
+            #endif
+        }
+
+        return mapToDevice(identifier: identifier)
+    }()
+
+}
+
 class BuyDetailViewController: UIViewController {
+    
+    var modelName = ""
     
     var itemTitle = ""
     var itemDescription = ""
@@ -92,6 +140,10 @@ class BuyDetailViewController: UIViewController {
         itemInWishlist = itemExistsInWishlist()
         
         //imageScrollView.frame = self.view.frame
+        
+        modelName = UIDevice.modelName
+        print("MODEL")
+        print(modelName)
         
         navigationItem.title = itemTitle
         itemPriceLabel.text = itemPrice
@@ -192,7 +244,24 @@ class BuyDetailViewController: UIViewController {
             imageView.image = imagesForView[i]
             imageView.contentMode = .scaleAspectFit
             let xPosition = self.view.frame.width * CGFloat(i)
-            imageView.frame = CGRect(x: xPosition, y: 0, width: 400, height: 300)
+            
+            //imageView.frame = CGRect(x: xPosition, y: 0, width: 400, height: 300)
+            if (modelName.contains("iPhone 11 Pro Max") ||
+                modelName.contains("iPhone XS Max")) {
+                imageView.frame = CGRect(x: xPosition, y: 0, width: 400, height: 300)
+            } else if (modelName.contains("iPhone 11 Pro") ||
+                modelName.contains("iPhone XS") ||
+                modelName.contains("iPhone X")) {
+                imageView.frame = CGRect(x: xPosition, y: 0, width: 370, height: 277)
+            } else if (modelName.contains("iPhone 11") ||
+                modelName.contains("iPhone XR")) {
+                imageView.frame = CGRect(x: xPosition, y: 0, width: 370, height: 277)
+            } else if (modelName.contains("iPhone 7 Plus") ||
+                modelName.contains("iPhone 8 Plus")) {
+                imageView.frame = CGRect(x: xPosition, y: 0, width: 300, height: 225)
+            } else {
+                imageView.frame = CGRect(x: xPosition, y: 0, width: 280, height: 210)
+            }
             
             self.imageScrollView.contentSize.width = 405 * CGFloat(i + 1) //self.imageScrollView.frame.width * CGFloat(i + 1)
             self.imageScrollView.addSubview(imageView)
@@ -241,7 +310,24 @@ class BuyDetailViewController: UIViewController {
             //imageView.contentMode = .scaleAspectFill
             let xPosition = self.view.frame.width * CGFloat(i)
             //imageView.frame = CGRect(x: xPosition, y: 0, width: self.imageScrollView.frame.width, height: self.imageScrollView.frame.height)
-            imageView.frame = CGRect(x: xPosition, y: 0, width: 400, height: 300)
+            
+            //imageView.frame = CGRect(x: xPosition, y: 0, width: 400, height: 300)
+            if (modelName.contains("iPhone 11 Pro Max") ||
+                modelName.contains("iPhone XS Max")) {
+                imageView.frame = CGRect(x: xPosition, y: 0, width: 400, height: 300)
+            } else if (modelName.contains("iPhone 11 Pro") ||
+                modelName.contains("iPhone XS") ||
+                modelName.contains("iPhone X")) {
+                imageView.frame = CGRect(x: xPosition, y: 0, width: 370, height: 277)
+            } else if (modelName.contains("iPhone 11") ||
+                modelName.contains("iPhone XR")) {
+                imageView.frame = CGRect(x: xPosition, y: 0, width: 370, height: 277)
+            } else if (modelName.contains("iPhone 7 Plus") ||
+                modelName.contains("iPhone 8 Plus")) {
+                imageView.frame = CGRect(x: xPosition, y: 0, width: 300, height: 225)
+            } else {
+                imageView.frame = CGRect(x: xPosition, y: 0, width: 280, height: 210)
+            }
             
             self.imageScrollView.contentSize.width = 405 * CGFloat(i + 1) //self.imageScrollView.frame.width * CGFloat(i + 1)
             self.imageScrollView.addSubview(imageView)
