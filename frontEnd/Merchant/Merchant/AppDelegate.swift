bbb6955bfc6d7bb5eb07521d864c8f44c39349b0
aakarshitpandey <aakarshit.pandey7@gmail.com>
2020-04-22 21:32:53 +0530

    Added support for notification when in active state

--- frontEnd/Merchant/Merchant/AppDelegate.swift
+++ frontEnd/Merchant/Merchant/AppDelegate.swift
@@ -116,7 +116,7 @@ extension AppDelegate : UNUserNotificationCenterDelegate {
     print(userInfo)
 
     // Change this to your preferred presentation option
-    completionHandler([])
+    completionHandler([.alert])
   }
 
   func userNotificationCenter(_ center: UNUserNotificationCenter,
