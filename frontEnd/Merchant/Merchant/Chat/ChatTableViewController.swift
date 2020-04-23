edf0e3acf3b26a9c23d18f87178661b2ccebc427
Drew Keirn <keirna@purdue.edu>
2020-04-23 09:37:36 -0700

    fixed delete chat on frontend

--- frontEnd/Merchant/Merchant/Chat/ChatTableViewController.swift
+++ frontEnd/Merchant/Merchant/Chat/ChatTableViewController.swift
@@ -64,7 +64,13 @@ class ChatTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            // TODO: Aakarshit: remove the chat in the database
+            
+            
+            self.users.remove(at: indexPath.row)
+            self.previews.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
