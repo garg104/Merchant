c077429dc1c7e5231e24a9f06432c84563ac04a4
aakarshitpandey <aakarshit.pandey7@gmail.com>
2020-04-24 15:12:33 +0530

    updated the route for sharing location

--- backEnd/routes/index.js
+++ backEnd/routes/index.js
@@ -7,6 +7,7 @@ const Item = require('../models/Items')
 const Conversations = require('../models/Conversations')
 const User = require('../models/User')
 const Location = require('../models/Location')
+const Pusher = require('pusher')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -17,6 +18,18 @@ router.get('/', function (req, res, next) {
 router.post('/meetingLocations', authenticate, async (req, res) => {
   const { conversationID, latitude, longitude, address, title, receiverUsername } = req.body
   const conversation = await Conversations.findById(conversationID)
+
+  //create a pusher instance
+  let pusher = new Pusher({
+    appId: '988508',
+    key: '0abb5543b425a847ea81',
+    secret: '28b34e176e9568cd6048',
+    cluster: 'us2',
+    encrypted: true
+  });
+
+  let channelName = req.userInfo.username + '-' + receiverUsername + '-maps'
+
   if (conversation) {
     try {
       const location = new Location({ latitude, longitude, address, title })
@@ -27,35 +40,12 @@ router.post('/meetingLocations', authenticate, async (req, res) => {
       const ret = await dispatchAPNViaFirebase(req.userInfo.username,
         receiverUsername,
         `${receiverUsername} has updated the meeting location`)
+      pusher.trigger(channelName, 'map-location', { "location": location });
       res.status(200).json({ location, msg: "Location has been saved" })
     } catch (e) {
       res.status(400).json({ msg: "Location couldn't be updated" })
     } //end try-catch
-  } else {
-    //in case wrong conversation id supplied
-    try {
-      const receiver = await User.findOne({ username: receiverUsername })
-      if (receiver) {
-        //finding the conversation using the id of users
-        const conversation = await Conversations.findOne({ userIDSender: req.userInfo._id, userIDReceiver: receiver._id })
-        if (conversation) {
-          //updating the new user location
-          const location = new Location({ latitude, longitude, address, title })
-          const savedLocation = await location.save()
-          conversation.meeting = savedLocation._id
-          await Conversations.findByIdAndUpdate(conversation._id, { ...conversation })
-          const ret = await dispatchAPNViaFirebase(req.userInfo.username,
-            receiverUsername,
-            `${receiverUsername} has updated the meeting location`)
-          res.status(200).json({ location, msg: "Location has been saved" })
-          //sending the push notification
-        }
-      }
-    } catch (e) {
-      console.log(e)
-      res.status(400).json({ msg: "Location couldn't be updated" })
-    }
-  } //end if
+  }
 })
 
 router.get('/safeLocations', authenticate, async (req, res) => {
